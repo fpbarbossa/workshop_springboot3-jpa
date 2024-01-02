2974a5b3c0fc72b7578a4d0dd81c0ee0ffc6ac04
Felipe Barbosa <fpbarbosa@live.com>
2024-01-02 18:27:56 -0300

    User entity and resource

---
+++ src/main/java/com/fpbarbosa/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.fpbarbosa.course.resources;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fpbarbosa.course.entites.User;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "Maria", "maria@gmail.com", "999999", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+}
