6fd868159e40671f89f27db22972f80580568bad
Felipe Barbosa <fpbarbosa@live.com>
2024-01-03 15:53:54 -0300

    User insert

--- src/main/java/com/fpbarbosa/course/resources/UserResource.java
+++ src/main/java/com/fpbarbosa/course/resources/UserResource.java
@@ -1,10 +1,12 @@
 package com.fpbarbosa.course.resources;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.fpbarbosa.course.entites.User;
 import com.fpbarbosa.course.services.UserService;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
@@ -31,5 +36,12 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id){
         User obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping //método post do http (inserir no banco de dados)
+    public ResponseEntity<User> insert(@RequestBody User obj){
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 }
--- src/main/java/com/fpbarbosa/course/services/UserService.java
+++ src/main/java/com/fpbarbosa/course/services/UserService.java
@@ -23,4 +23,8 @@ public class UserService {
         Optional<User> obj = repository.findById(id);
         return obj.get();
     }
+
+    public User insert(User obj){ //Inserir no Banco de dados um novo obj do tipo user
+        return repository.save(obj);
+    }
 }
