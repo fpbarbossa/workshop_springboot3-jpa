3be74c405aa1020dd7548a5eb7b2b4460599f9eb
Felipe Barbosa <fpbarbosa@live.com>
2024-01-03 16:57:31 -0300

    Exception Handler findById

---
+++ src/main/java/com/fpbarbosa/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.fpbarbosa.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(Object id){
+        super("Resource not found. ID " + id);
+    }
+}
