061e9e0675a948a197fa93430f0b6c71e47db391
Felipe Barbosa <fpbarbosa@live.com>
2024-01-03 09:47:41 -0300

    Category

---
+++ src/main/java/com/fpbarbosa/course/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.fpbarbosa.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fpbarbosa.course.entites.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+} 
